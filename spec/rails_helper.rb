86cbaf97cda91ecdf7ef2e9b54a49f94b1ddafa9
warrenbrown <warren@katapultmedia.com>
2017-01-02 10:50:35 -0600

    added ability for user to sign out

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,6 +52,6 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  config.include Warden::Test::Helpers, type: :feature
+  config.after(type: :feature) { Warden.test_reset! }
 end
