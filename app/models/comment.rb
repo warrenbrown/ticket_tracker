6a746ac65fef34c03375e5701a55e987e71c0be9
warrenbrown <warren@katapultmedia.com>
2017-01-06 16:14:09 -0600

    Display a comments state transition

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,11 @@
 class Comment < ActiveRecord::Base
+  before_create :set_previous_state
   after_create :set_ticket_state
 
   belongs_to :state
   belongs_to :ticket
   belongs_to :author, class_name: 'User'
+  belongs_to :previous_state, class_name: "State"
 
   validates :text, presence: true
 
@@ -12,10 +14,14 @@ class Comment < ActiveRecord::Base
   scope :persisted, lambda { where.not(id: nil) }
 
   private
-
+    def set_previous_state
+      self.previous_state = ticket.state
+    end
 
     def set_ticket_state
       ticket.state = state
       ticket.save!
     end
+
+
 end
